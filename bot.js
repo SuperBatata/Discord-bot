2fdebcf51c97cc50bd76c6e93e60b77f09f915cc
SiBatata <khaled.battiche@esprit.tn>
2020-12-13 18:41:54 +0100

    bot can react

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('message', msg => {
 });
 
 client.on("message", msg => {
-  if (msg.content == "love health") {
+  if (msg.content == "i love health bot") {
     msg.react("❤️");
   }});
 
