c13cc0500506523fc37bd168bdd0c8681acd6cdf
SiBatata <khaled.battiche@esprit.tn>
2020-12-13 12:42:40 +0100

    bot

---
+++ bot.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '!bot') {
+    msg.reply('waa sahbé!');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
